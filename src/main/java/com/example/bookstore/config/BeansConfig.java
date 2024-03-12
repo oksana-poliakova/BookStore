fcbe52995a6c6ce69fdb580b62ec693e6aa41c85
Oksana Poliakova <it.oksana.poliakova@gmail.com>
2024-03-12 11:45:09 +0200

    Initial commit

---
+++ src/main/java/com/example/bookstore/config/BeansConfig.java
@@ -0,0 +1,5 @@
+package com.example.bookstore.config;/**
+ * @projectName BookStore
+ * @author oksanapoliakova on 12.03.2024
+ */public class BeansConfig {
+}
---
+++ src/main/java/com/example/bookstore/entity/Book.java
@@ -0,0 +1,5 @@
+package com.example.bookstore.entity;/**
+ * @projectName BookStore
+ * @author oksanapoliakova on 12.03.2024
+ */public class Book {
+}
