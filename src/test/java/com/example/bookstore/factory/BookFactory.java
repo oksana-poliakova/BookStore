1134dae9a9ff4b3240222dfc37161b749b9986dd
Oksana Poliakova <it.oksana.poliakova@gmail.com>
2024-03-13 14:38:35 +0200

    Added BookFactory

---
+++ src/test/java/com/example/bookstore/factory/BookFactory.java
@@ -0,0 +1,33 @@
+package com.example.bookstore.factory;
+
+import com.example.bookstore.entity.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author oksanapoliakova on 13.03.2024
+ * @projectName BookStore
+ */
+public class BookFactory {
+    public static List<Book> createBooks() {
+        List<Book> books = new ArrayList<>();
+
+        Book book1 = createBook("Book1", "Author1", "Description1", 10.00);
+        Book book2 = createBook("Book2", "Author2", "Description2", 20.00);
+
+        books.add(book1);
+        books.add(book2);
+
+        return books;
+    }
+
+    private static Book createBook(String name, String author, String description, double price) {
+        Book book = new Book();
+        book.setName(name);
+        book.setAuthor(author);
+        book.setDescription(description);
+        book.setPrice(price);
+        return book;
+    }
+}
